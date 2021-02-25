14606b68deb7969a67d15434bcec4e01485526da
Toni10 <toni.djordjevic91@gmail.com>
2021-02-25 16:05:08 +0100

    Add project files.

---
+++ Cas31/PageObjects/LoginPage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Cas31.PageObjects
+{
+    class LoginPage : BasePage
+    {
+
+        public LoginPage(IWebDriver driver) : base(driver) { }
+
+        public IWebElement inputUsername
+        {
+            get
+            {
+                return this.FindElement(By.Name("username"));
+            }
+        }
+
+        public IWebElement inputPassword
+        {
+            get
+            {
+                return this.FindElement(By.Name("password"));
+            }
+        }
+
+        public IWebElement buttonLogin
+        {
+            get
+            {
+                return this.FindElement(By.Name("login"));
+            }
+        }
+
+        public void EnterUsername(string username)
+        {
+            this.inputUsername.SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            this.inputPassword.SendKeys(password);
+        }
+
+        public HomePage ClikOnButtonLogin()
+        {
+            this.buttonLogin.Click();
+            try
+            {
+                this.waitElementToBeVisible(By.XPath("//h2[contains(text(), 'Welcome back,')]"));
+            } catch (WebDriverTimeoutException)
+            {
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+            this.ExplicitWait(500);
+            return new HomePage(this.driver);
+        }
+    }
+}
---
+++ Cas31/PageObjects/RegisterPage.cs
@@ -0,0 +1,106 @@
+﻿using OpenQA.Selenium;
+
+namespace Cas31.PageObjects
+{
+    class RegisterPage : BasePage
+    {
+        public RegisterPage(IWebDriver driver) : base(driver) { }
+
+        private IWebElement inputFirstName
+        {
+            get
+            {
+                return this.FindElement(By.Name("ime"));
+            }
+        }
+
+        private IWebElement inputLastName
+        {
+            get
+            {
+                return this.FindElement(By.Name("prezime"));
+            }
+        }
+
+        private IWebElement inputEmail
+        {
+            get
+            {
+                return this.FindElement(By.Name("email"));
+            }
+        }
+
+        private IWebElement inputUsername
+        {
+            get
+            {
+                return this.FindElement(By.Name("korisnicko"));
+            }
+        }
+
+        private IWebElement inputPassword
+        {
+            get
+            {
+                return this.FindElement(By.Name("lozinka"));
+            }
+        }
+
+        private IWebElement inputPasswordRepeat
+        {
+            get
+            {
+                return this.FindElement(By.Name("lozinkaOpet"));
+            }
+        }
+
+        private IWebElement buttonRegister
+        {
+            get
+            {
+                return this.FindElement(By.Name("register"));
+            }
+        }
+
+        public void EnterFirstName(string firstName)
+        {
+            this.inputFirstName.SendKeys(firstName);
+        }
+
+        public void EnterLastName(string lastName)
+        {
+            this.inputLastName.SendKeys(lastName);
+        }
+
+        public void EnterEmail(string email)
+        {
+            this.inputEmail.SendKeys(email);
+        }
+
+        public void EnterUsername(string username)
+        {
+            this.inputUsername.SendKeys(username);
+        }
+
+        public void EnterPassword(string password)
+        {
+            this.inputPassword.SendKeys(password);
+        }
+
+        public void EnterPasswordAgain(string password)
+        {
+            this.inputPasswordRepeat.SendKeys(password);
+        }
+
+        public HomePage ClickOnButtonRegister()
+        {
+            this.buttonRegister.Click();
+            this.waitElementToBeVisible(
+                By.XPath("//div[contains(@class, 'success') and contains(., 'Uspeh')]")
+            );
+            this.ExplicitWait(500);
+            return new HomePage(this.driver);
+        }
+
+    }
+}
